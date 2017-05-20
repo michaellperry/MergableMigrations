6ce82cacf098ca4e74497f2e696a0aaee1fb6ead
Michael L Perry <michael@qedcode.com>
2017-05-20 13:37:06 -0500

    Switched to Bouncy Castle beause it supports .NET Standard 1.4.

--- MergableMigrations.Specification/Implementation/StringHash.cs
+++ MergableMigrations.Specification/Implementation/StringHash.cs
@@ -1,16 +1,20 @@
-﻿using System.Security.Cryptography;
+﻿using Org.BouncyCastle.Crypto.Digests;
+using System;
 using System.Text;
 
 namespace MergableMigrations.Specification.Implementation
 {
     static class StringHashExtensions
     {
-        private static SHA256 Sha256Alrorithm = SHA256.Create();
-
         public static int Sha356Hash(this string str)
         {
-            var bytes = Sha256Alrorithm.ComputeHash(Encoding.UTF8.GetBytes(str));
-            return System.BitConverter.ToInt32(bytes, 0);
+            var digest = new Sha256Digest();
+            byte[] stringBytes = Encoding.UTF8.GetBytes(str);
+            digest.BlockUpdate(stringBytes, 0, stringBytes.Length);
+            digest.Finish();
+            byte[] bytes = new byte[digest.GetByteLength()];
+            digest.DoFinal(bytes, 0);
+            return BitConverter.ToInt32(bytes, 0);
         }
     }
 }
